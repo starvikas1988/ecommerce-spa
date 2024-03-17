bff6b056b65631a4d3ae2bea26e89f7af76fe7ae
starvikass1988 <starvikass@gmail.com>
2024-03-17 21:53:44 +0530

    header updated

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -85,9 +85,7 @@
                     </button>
                     <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div class="navbar-nav mx-auto">
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="shop.html" class="nav-item nav-link">Shop</a>
-                            <a href="shop-detail.html" class="nav-item nav-link">Shop Detail</a>
+                            <a href="{{route('products.index')}}" class="nav-item nav-link active">Home</a>
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                                 <div class="dropdown-menu m-0 bg-secondary rounded-0">
